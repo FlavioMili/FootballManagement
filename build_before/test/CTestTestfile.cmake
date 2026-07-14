c3c53278067ee31be4143cd5c25e1133a75a9a57
FlavioMili <flaviomilinanni@gmail.com>
2026-07-14 15:07:37 +0200

    chore: cleanup build artifacts and temp files

--- build_before/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Flavio/Programming/FootballManagement/test
-# Build directory: /home/Flavio/Programming/FootballManagement/build_before/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/Flavio/Programming/FootballManagement/build_before/test/unit_tests_e3b0c442_include.cmake")
